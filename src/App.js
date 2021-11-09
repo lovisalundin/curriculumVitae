87358e00ccadb3e939073dea6e79593b0c26e85c
Lovisa Lundin <lovisa.lundin@doktor24.se>
2021-11-09 22:54:30 +0100

    setup initial structure and started CV

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Title} from './Title';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+
+      <a
+        className="App-link"
+        href="mailto:lovisa.c.lundin@gmail.com"
+      >
+        Mail
+      </a>
+      <a
+        className="App-link"
+        href="https://www.linkedin.com/in/lovisa-lundin-791128b5/"
+      > 
+        LinkedIn
+      </a>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import './Title.css'
+
+export function Title() {
+    return (
+        <div>
+            <h1 className="Title">Lovisa Lundin</h1>
+            <i className="Subtitle">M. Sc. Biomedical Engineering</i>
+        </div>
+    )
+}
