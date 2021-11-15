54ac956172d5a3bef0ea75588517734d36040408
Lovisa Lundin <lovisa.lundin@doktor24.se>
2021-11-15 22:41:52 +0100

    First column with Work experience

--- src/ColumnOne.js
+++ src/ColumnOne.js
@@ -1,9 +1,42 @@
-import '.ColumnOne.css';
+import './ColumnOne.css';
 
 export function ColumnOne() {
     return (
-        <div>
+        <div className="container">
+            <div className="column left">
+                <h1 className="header"> WORK EXPERIENCE </h1>
+                    <h3 className="timeline"> JANUARI 2020 - PRESENT </h3>
+                    <h3 className="employer"> Platform24 Healthcare AB </h3>
+                    <h2 className="title"> Product Manager </h2>
+                    <p className="description"> Product Manager for the Triage24 product which triages patients based on their symptoms and recommends appropriate level of care based on business operating rules </p>
 
+                    <h3 className="timeline"> JANUARI 2020 - PRESENT </h3>
+                    <h3 className="employer"> Platform24 Healthcare AB </h3>
+                    <h2 className="title"> Product Manager </h2>
+                    <p className="description"> Product Manager for the Triage24 product which triages patients based on their symptoms and recommends appropriate level of care based on business operating rules </p>
+
+                    <h3 className="timeline"> JANUARI 2020 - PRESENT </h3>
+                    <h3 className="employer"> Platform24 Healthcare AB </h3>
+                    <h2 className="title"> Product Manager </h2>
+                    <p className="description"> Product Manager for the Triage24 product which triages patients based on their symptoms and recommends appropriate level of care based on business operating rules </p>
+
+                    <h3 className="timeline"> JANUARI 2020 - PRESENT </h3>
+                    <h3 className="employer"> Platform24 Healthcare AB </h3>
+                    <h2 className="title"> Product Manager </h2>
+                    <p className="description"> Product Manager for the Triage24 product which triages patients based on their symptoms and recommends appropriate level of care based on business operating rules </p>
+
+                    <h3 className="timeline"> JANUARI 2020 - PRESENT </h3>
+                    <h3 className="employer"> Platform24 Healthcare AB </h3>
+                    <h2 className="title"> Product Manager </h2>
+                    <p className="description"> Product Manager for the Triage24 product which triages patients based on their symptoms and recommends appropriate level of care based on business operating rules </p>
+                <h1 className="header">EDUCATION </h1>
+
+        </div>
+            <div className="column right">
+                <p>
+                    då
+                </p>
+            </div>    
         </div>
     )
 }
